95d3ad9107114f74a13d538ce323d090c0e3dddf
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2023-09-16 22:40:09 +0530

    Fibonacci Problem Solved

---
+++ java-refesher/Fibonacci.java
@@ -0,0 +1,27 @@
+/*
+Assigment Question
+7. To calculate Fibonacci Series up to n numbers.
+*/
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter fibonacci count: ");
+    int count = input.nextInt();
+    int result, num1 = 0;
+    int num2 = 1;
+    System.out.print(num1 + "  ");
+    System.out.print(num2 + "  ");
+
+    for (int i = 0; i < count - 2; i++) {
+      result = num1 + num2;
+      num1 = num2;
+      num2 = result;
+      System.out.print(result + "  ");
+    }
+  }
+}
--- java-refesher/InrUsd.java
+++ java-refesher/InrUsd.java
@@ -10,7 +10,7 @@ public class InrUsd {
   public static void main(String args[]) {
     Scanner input = new Scanner(System.in);
     final float RATE = 84.45f;
-    System.out.print("Enter Amount (in INR): ");
+    System.out.print("Enter Amount (in INR): Rs.");
     float inrAmount = input.nextFloat();
     float usdAmount = inrAmount / RATE;
     System.out.println("After Conversion: $" + usdAmount);
