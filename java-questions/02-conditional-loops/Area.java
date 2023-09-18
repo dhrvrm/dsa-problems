2167ef5d7cd692db83a08fc1bba35ee6f5e6958c
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2023-09-18 11:59:10 +0530

    Perimeter Problem Solved

--- java-questions/02-conditional-loops/Area.java
+++ java-questions/02-conditional-loops/Area.java
@@ -17,45 +17,47 @@ public class Area {
     Scanner input = new Scanner(System.in);
 
     areaOfCircle(5);
-    areaOfTriange(5,6);
-    areaOfIsoscelesTriangle(8,6);
+    areaOfTriange(5, 6);
+    areaOfIsoscelesTriangle(8, 6);
     areaOfEquilateralTriangle(5);
-    areaOfRectangle(10,20);
-    areaOfParallelogram(10,15);
-    areaOfRhombus(25,23);
-
-    
-  }
-  static void areaOfCircle(double r){
-    final double PI=3.14;
-    double area=PI*(r*r);
-    System.out.println("Area of Circle: "+area);
+    areaOfRectangle(10, 20);
+    areaOfParallelogram(10, 15);
+    areaOfRhombus(25, 23);
   }
 
-  static void areaOfTriange(double height, double base){
-    double area=(height*base)/2;
-    System.out.println("Area of Triangle: "+area);
+  static void areaOfCircle(double r) {
+    final double PI = 3.14;
+    double area = PI * (r * r);
+    System.out.println("Area of Circle: " + area);
   }
 
-  static void areaOfIsoscelesTriangle(double height, double base){
-    double area=(height*base)/2;
-    System.out.println("Area of Isosceles Triangle: "+area);
+  static void areaOfTriange(double height, double base) {
+    double area = (height * base) / 2;
+    System.out.println("Area of Triangle: " + area);
   }
-  static void areaOfEquilateralTriangle(double side){
-    double area=(Math.sqrt(3)/4)*(side*side);
-    System.out.println("Area of Equilateral Triangle: "+area);
-  }
-  static void areaOfRectangle(double length, double width){
-    double area=length*width;
-    System.out.println("Area of Rectangle: "+area);
-  }
-  static void areaOfParallelogram(double height, double base){
-    double area=height*base;
-    System.out.println("Area of Parallelogram: "+area);
-  }
-  static void areaOfRhombus(double diagonal1, double diagonal2){
-    double area=(diagonal1*diagonal2)/2;
-    System.out.println("Area of Rhombus: "+area);
 
+  static void areaOfIsoscelesTriangle(double height, double base) {
+    double area = (height * base) / 2;
+    System.out.println("Area of Isosceles Triangle: " + area);
+  }
+
+  static void areaOfEquilateralTriangle(double side) {
+    double area = (Math.sqrt(3) / 4) * (side * side);
+    System.out.println("Area of Equilateral Triangle: " + area);
+  }
+
+  static void areaOfRectangle(double length, double width) {
+    double area = length * width;
+    System.out.println("Area of Rectangle: " + area);
+  }
+
+  static void areaOfParallelogram(double height, double base) {
+    double area = height * base;
+    System.out.println("Area of Parallelogram: " + area);
+  }
+
+  static void areaOfRhombus(double diagonal1, double diagonal2) {
+    double area = (diagonal1 * diagonal2) / 2;
+    System.out.println("Area of Rhombus: " + area);
   }
 }
---
+++ java-questions/02-conditional-loops/Perimeter.java
@@ -0,0 +1,57 @@
+/*
+Assigment Question Basic
+- Perimeter Of Circle
+- Perimeter Of Equilateral Triangle
+- Perimeter Of Parallelogram
+- Perimeter Of Rectangle
+- Perimeter Of Square
+- Perimeter Of Rhombus
+*/
+
+import java.util.Scanner;
+
+public class Perimeter {
+
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+
+    perimeterOfCircle(5);
+    perimeterOfEquilateralTriangle(5);
+    perimeterOfParallelogram(10, 15);
+    perimeterOfRectangle(10, 20);
+    perimeterOfSquare(5);
+    perimeterOfRhombus(25);
+  }
+
+  static void perimeterOfCircle(double r) {
+    final double PI = 3.14;
+    double perimeter = 2 * PI * r;
+    System.out.println("Perimeter of Circle: " + perimeter);
+  }
+
+  static void perimeterOfEquilateralTriangle(double side) {
+    double perimeter = 3 * side;
+    System.out.println("Perimeter of Triangle: " + perimeter);
+  }
+
+  static void perimeterOfSquare(double side) {
+    double perimeter = 4 * side;
+    System.out.println("Perimeter of Square: " + perimeter);
+  }
+
+
+  static void perimeterOfRectangle(double length, double width) {
+    double perimeter = 2 * (length + width);
+    System.out.println("Perimeter of Rectangle: " + perimeter);
+  }
+
+  static void perimeterOfParallelogram(double height, double base) {
+    double perimeter = 2 * (height + base);
+    System.out.println("Perimeter of Parallelogram: " + perimeter);
+  }
+
+  static void perimeterOfRhombus(double side) {
+    double perimeter = 4 * side;
+    System.out.println("Perimeter of Rhombus: " + perimeter);
+  }
+}
