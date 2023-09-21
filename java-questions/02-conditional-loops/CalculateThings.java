5714610c6e7466a2e2d03d60b380805e0f998d6e
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2023-09-21 22:26:53 +0530

    Calculation Problems
    Discount and Distance

--- java-questions/02-conditional-loops/CalculateThings.java
+++ java-questions/02-conditional-loops/CalculateThings.java
@@ -19,7 +19,9 @@ public class CalculateThings {
     Scanner input = new Scanner(System.in);
 
     calculateElectrictyBill(167);
-    calculateAverage(new int[] {2,3,5,7});
+    calculateAverage(new int[] { 2, 3, 5, 7 });
+    calculateDiscount(1000, 20f);
+    calculateDistance(3,5,5,3);
   }
 
   static void calculateElectrictyBill(double units) {
@@ -47,5 +49,19 @@ public class CalculateThings {
       average = sum / nums.length;
     }
     System.out.println("Average: " + average);
+  }
+
+  static void calculateDiscount(int amount, float percentage) {
+    float dAmount = amount * ((100 - percentage) / 100);
+    System.out.println(
+      amount + " after " + percentage + "% Discount : " + dAmount
+    );
+  }
+
+  static void calculateDistance(int x1, int y1, int x2, int y2) {
+    System.out.println(
+      "Distance Between Points " +
+      Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) * 1.0)
+    );
   }
 }
