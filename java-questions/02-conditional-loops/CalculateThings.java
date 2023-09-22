fb28fe6c64d8ef6ce8a80b172ac2c257e9dca7f5
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2023-09-22 11:41:37 +0530

    Calculation problem solved
    Calculate commission, depreciation, batting average, CGPA, average marks

--- java-questions/02-conditional-loops/CalculateThings.java
+++ java-questions/02-conditional-loops/CalculateThings.java
@@ -21,7 +21,12 @@ public class CalculateThings {
     calculateElectrictyBill(167);
     calculateAverage(new int[] { 2, 3, 5, 7 });
     calculateDiscount(1000, 20f);
-    calculateDistance(3,5,5,3);
+    calculateDistance(3, 5, 5, 3);
+    calculateCommission(2000, 12.2f);
+    calculateDepriciation(110000, 10.0f);
+    calculateBattingAverage(350, 4);
+    calculateCGPA(80.0f);
+    calculateAverageMarks(new int[] {45,56,49,78,68,86,31});
   }
 
   static void calculateElectrictyBill(double units) {
@@ -63,5 +68,53 @@ public class CalculateThings {
       "Distance Between Points " +
       Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) * 1.0)
     );
+  }
+
+  static void calculateCommission(double amount, float percentage) {
+    System.out.println(
+      "Commition on sales of " +
+      amount +
+      " is: " +
+      (amount * (percentage / 100))
+    );
+  }
+
+  static void calculateDepriciation(double amount, float percentage) {
+    System.out.println(
+      "Depriciation on original value of " +
+      amount +
+      " is: " +
+      (amount * (percentage / 100))
+    );
+  }
+
+  static void calculateBattingAverage(int runScored, int timesOut) {
+    System.out.println(
+      "Batting Average of a player with " +
+      runScored +
+      " total run scored and out " +
+      timesOut +
+      " times is: " +
+      (runScored / timesOut)
+    );
+  }
+
+  static void calculateCGPA(float percentage) {
+    final float RATE = 9.5f;
+    System.out.println(
+      "Percentage " + percentage + " % to CGPA is: " + (percentage / RATE)
+    );
+  }
+
+  static void calculateAverageMarks(int[] marks) {
+    int sum = 0;
+    float average = 0f;
+    if (marks.length > 0) {
+      for (int i = 0; i < marks.length; i++) {
+        sum += marks[i];
+      }
+      average = sum / marks.length;
+    }
+    System.out.println("Average Marks: " + average);
   }
 }
