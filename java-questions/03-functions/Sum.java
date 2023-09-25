ae6412a3153b5469db8a074c37c5a5ba505618d5
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2023-09-25 21:53:30 +0530

    Sum Function Solved

---
+++ java-questions/03-functions/Sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Sum {
+
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter two numbers:");
+    int num1 = in.nextInt();
+    int num2 = in.nextInt();
+    sum(num1,num2);
+  }
+
+  static void sum(int num1,int num2) {
+
+      System.out.println("Sum: "+(num1+num2));
+  }
+}
